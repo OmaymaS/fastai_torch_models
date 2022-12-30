89fd6e0d1c37c6c2f261e7b5b7cee2facb6039f7
osaid <13340411+OmaymaS@users.noreply.github.com>
2022-12-30 12:25:38 +0100

    modify traing and logging

--- fastai_training_job/train_job_image/train.py
+++ fastai_training_job/train_job_image/train.py
@@ -3,105 +3,160 @@ import os
 import time
 
 import fire
-import numpy as np
 import pandas as pd
 from fastai.vision.all import *
 
-from gcp_utils import publish_metrics
+from test_model_utils import *
 
-timestamp = time.strftime("%Y%m%d-%H%M%S")
+training_timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+TEST_THR_LIST = [x / 10 for x in range(11)]
 
 
-def train_evaluate(job_dir: str = None,
-                   training_dataset_path: str = None,
-                   training_images_path: str = None,
-                   model_version: str = None,
-                   model_hyperparms_path: str = None):
+def train_evaluate(
+    job_dir: str = None,
+    training_dataset_path: str = None,
+    training_images_path: str = None,
+    model_version: str = None,
+    model_config_path: str = None,
+    test_step: bool = False,
+    testing_dataset_path: str = None,
+    testing_images_path: str = None,
+):
 
     if torch.cuda.is_available():
-        print('GPU available')
+        print("GPU available")
 
-    # GCS gets mounted when the instance starts using gcsfuse. All prefixes exist under /gcs/
-    job_dir, training_images_path, model_hyperparms_path = [f.replace(
-        'gs://', '/gcs/') if f.startswith('gs') else f for f in [job_dir, training_images_path, model_hyperparms_path]]
+    ## GCS gets mounted when the instance starts using gcsfuse. All prefixes exist under /gcs/
+    job_dir, training_images_path, model_config_path = [
+        f.replace("gs://", "/gcs/") if f.startswith("gs://") else f
+        for f in [job_dir, training_images_path, model_config_path]
+    ]
 
-    # create subdir/prefix to export results
-    job_subdir_export = f'{job_dir}/{model_version}_{timestamp}'
-    os.makedirs(job_subdir_export)
+    ## create subdir/prefix to export results
+    job_subdir_gcs_export = f"{job_dir}/{model_version}_{training_timestamp}"
+    os.makedirs(job_subdir_gcs_export)
 
-    # read hyperparams values
-    with open(model_hyperparms_path) as model_hparams_json:
-        model_hparams_dict = json.load(model_hparams_json)
-        print(f'hyperparameters: {model_hparams_dict}')
+    ## read config values
+    with open(model_config_path) as model_config_json:
+        model_config_dict = json.load(model_config_json)
+        print(f"hyperparameters: {model_config_dict}")
 
-    # read training data csv
-    print('Reading csv ...')
+    ## include specified tags if available, otherwise sue all
+    if "TAGS" in model_config_dict:
+        TAG_LIST = [
+            included_tag.strip()
+            for included_tag in model_config_dict["TAGS"].split(",")
+        ]
+    else:
+        TAG_LIST = None
+
+    ## read training data csv
+    print("Reading csv ...")
     df_train = pd.read_csv(training_dataset_path)
+    if TAG_LIST:
+        df_train = df_train[df_train["tag"].isin(TAG_LIST)]
 
-    print('Loading and transforming images...')
-    # define datablock
-    img_block_01 = DataBlock(blocks=(ImageBlock, MultiCategoryBlock),
-                             splitter=ColSplitter('is_valid'),
-                             get_x=ColReader(
-                                 'image_id', pref=f'{training_images_path}/', suff=''),
-                             get_y=ColReader(
-                                 model_hparams_dict['TAG_COLUMN'], label_delim=','),
-                             item_tfms=Resize(
-                                 model_hparams_dict['RESIZE_VALUE'], method=model_hparams_dict['METHOD']),
-                             batch_tfms=aug_transforms(do_flip=False))
+    print(f"number of rows in train data: {len(df_train)}")
 
-    # load images and transform
-    img_dls_01 = img_block_01.dataloaders(df_train)
-
-    # train
-    print('Starting training...')
-    fscore = F1ScoreMulti()
-    learn_01 = cnn_learner(dls=img_dls_01,
-                           arch=eval(model_hparams_dict['ARCH_RESNET']),
-                           metrics=[accuracy_multi, fscore])
-    learn_01.fine_tune(
-        model_hparams_dict['EPOCHS'],
-        base_lr=model_hparams_dict['BASE_LR'],
-        freeze_epochs=model_hparams_dict['FREEZE_EPOCHS'],
-        cbs=[SaveModelCallback(monitor='accuracy_multi',
-                               fname='model', with_opt=True)]
+    print("Loading and transforming images...")
+    ## define datablock
+    img_block = DataBlock(
+        blocks=(ImageBlock, MultiCategoryBlock),
+        splitter=ColSplitter("is_valid"),
+        get_x=ColReader("image_id", pref=f"{training_images_path}/", suff=""),
+        get_y=ColReader(model_config_dict["TAG_COLUMN"], label_delim=","),
+        item_tfms=Resize(
+            model_config_dict["RESIZE_VALUE"], method=model_config_dict["METHOD"]
+        ),
+        batch_tfms=aug_transforms(do_flip=False),
     )
-    print('Training completed.')
 
-    # dict to add metrics
+    ## load images and transform
+    img_dls = img_block.dataloaders(df_train)
+
+    ## train
+    print("Starting training...")
+    fscore = F1ScoreMulti()
+    model_multi = cnn_learner(
+        dls=img_dls,
+        arch=eval(model_config_dict["ARCH_RESNET"]),
+        metrics=[accuracy_multi, fscore],
+    )
+    model_multi.fine_tune(
+        model_config_dict["EPOCHS"],
+        base_lr=model_config_dict["BASE_LR"],
+        freeze_epochs=model_config_dict["FREEZE_EPOCHS"],
+        cbs=[SaveModelCallback(monitor="accuracy_multi", fname="model", with_opt=True)],
+    )
+    print("Training completed.")
+
+    ## dict to add metrics
     metrics_log = {}
 
-    # get final model metrics
-    # here accuracy_multi is maximized (can be changed to any other metric like valid_loss)
-    accuracy_multi_train = learn_01.recorder.metrics[0].value.item()
-    metrics_log['accuracy_multi_train'] = accuracy_multi_train
+    ## get final model metrics
+    accuracy_multi_train = model_multi.recorder.metrics[0].value.item()
 
-    # save metrics
+    ## TEST DATA METRICS -----------------------------------
+    ## if test data is provided and test_step is True
+    if test_step:
+        print("Evaluating model on test data")
+        if testing_images_path.startswith("gs://"):
+            testing_images_path = testing_images_path.replace("gs://", "/gcs/")
+
+        metrics_test = predict_calculate_classification_metrics(
+            model_trained=model_multi,
+            testing_images_path=testing_images_path,
+            testing_dataset_path=testing_dataset_path,
+            tag_column=model_config_dict["TAG_COLUMN"],
+            tag_list=TAG_LIST,
+            test_threshold_list=TEST_THR_LIST,
+        )
+
+        ## append metrics to log
+        metrics_log["test_metrics"] = metrics_test
+        metrics_log["test_dataset"] = testing_dataset_path
+        metrics_log["train_timestamp"] = training_timestamp
+        metrics_log["train_metrics"] = {
+            "fasti_metrics": {"accuracy_multi_train_default": accuracy_multi_train}
+        }
+    else:
+        metrics_log = {
+            "fasti_metrics": {"accuracy_multi_train_default": accuracy_multi_train}
+        }
+    ## -------------------------------------------------------------
+
+    ## save metrics
     print("Saving metrics")
-    with open(f'{job_subdir_export}/metrics_log.json', "w") as f:
+    metrics_log_path = f"{job_subdir_gcs_export}/metrics_log.json"
+    with open(metrics_log_path, "w") as f:
         json.dump(metrics_log, f)
-    print(metrics_log)
 
-    # pickle model ------
-    model_name = f'model_{model_version}'
+    ## save config
+    with open(f"{job_subdir_gcs_export}/model_config.json", "w") as f:
+        json.dump(model_config_dict, f)
 
-    pickle_model_path = f'{job_subdir_export}/{model_name}.pkl'
-    print(f'Saving pickle model to {pickle_model_path}')
-    learn_01.export(f'{pickle_model_path}')
+    ## save model
+    model_multi.version = model_version  ## add model version to saved model
+    model_name = f"model_{model_version}.pkl"
+    model_path = f"{job_subdir_gcs_export}/{model_name}"
+    print(f"Saving model to {model_path}")
+    model_multi.export(f"{model_path}")
 
-    # jit model ----------
-    # save for native torch import later
-    dummy_inp = torch.randn(
-        [1, 3, model_hparams_dict['RESIZE_VALUE'], model_hparams_dict['RESIZE_VALUE']])  # dummy
+    ## save job details
+    job_inputs_log = {
+        "job_dir": job_dir,
+        "training_dataset_path": training_dataset_path,
+        "training_images_path": training_images_path,
+        "model_config_path": training_images_path,
+        "test_step": test_step,
+        "testing_dataset_path": testing_dataset_path,
+        "testing_images_path": testing_dataset_path,
+    }
 
-    jit_model_path = f'{job_subdir_export}/{model_name}.pt'
-    print(f'Saving jit model to {jit_model_path}')
-    torch.jit.save(torch.jit.trace(learn_01.model, dummy_inp),
-                   f'{job_subdir_export}/model_{model_version}.pt')
-
-    # save vocab with jit model
-    vocab = np.save('models/vocab.npy', learn_01.dls.vocab)
-
+    job_inputs_json_path = f"{job_subdir_gcs_export}/job_inputs_log.json"
+    with open(job_inputs_json_path, "w") as f:
+        json.dump(job_inputs_log, f)
 
 if __name__ == "__main__":
     fire.Fire(train_evaluate)
